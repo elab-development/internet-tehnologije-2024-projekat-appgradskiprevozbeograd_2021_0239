809f246e5d6044ca26b85c57dae21f04e33eedc2
Mihajlo Djordjevic <mimi.djordjevic23@gmail.com>
2025-12-08 15:54:40 +0100

    Dodata logika za registraciju i logovanje

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+<<<<<<< HEAD
+=======
+use App\Http\Controllers\Auth\AuthController;
+>>>>>>> e4356dd (Dodata logika za registraciju i logovanje)
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+<<<<<<< HEAD
+=======
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+>>>>>>> e4356dd (Dodata logika za registraciju i logovanje)
